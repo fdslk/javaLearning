198c42ed2086cb1ecc0dd5b1bbf9c2b7153ae87e
zengqiang.fang <zengqiang.fang@thougthworks.com>
2022-01-02 11:56:14 +0800

    修复unit test
    Signed-off-by: zengqiang.fang <zengqiang.fang@thougthworks.com>

--- code/demo/src/test/java/com/example/demo/TestControllerTests.java
+++ code/demo/src/test/java/com/example/demo/TestControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
@@ -16,15 +17,15 @@ public class TestControllerTests extends ApiSetup {
 
         String url = String.format("/hello?myName=%s",param);
 
-//        var result = this.testClient
-//                .get()
-//                .uri(url)
-//                .exchange()
-//                .expectStatus()
-//                .is2xxSuccessful()
-//                .expectBody(String.class)
-//                .returnResult();
-//        Assertions.assertEquals(result.getResponseBody(), expectedResult);
+        EntityExchangeResult<String> stringEntityExchangeResult = this.testClient
+                .get()
+                .uri(url)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .returnResult();
+        Assertions.assertEquals(stringEntityExchangeResult.getResponseBody(), expectedResult);
     }
 
     @Test
@@ -32,14 +33,14 @@ public class TestControllerTests extends ApiSetup {
         mockClient.when(request().withPath("/baidu.com"))
                 .respond(response().withStatusCode(200).withBody("test"));
 
-//        var result = this.testClient
-//                .get()
-//                .uri("/helloBaidu")
-//                .exchange()
-//                .expectStatus()
-//                .is2xxSuccessful()
-//                .expectBody(String.class)
-//                .returnResult();
-//        Assertions.assertEquals(result.getResponseBody(), "test");
+        EntityExchangeResult<String> result = this.testClient
+                .get()
+                .uri("/helloBaidu")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .returnResult();
+        Assertions.assertEquals(result.getResponseBody(), "test");
     }
 }
