38cffe34f3458f8e34da6bcdbf193056bce552f1
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-01-06 20:29:54 +0800

    添加接口测试global exception handler
    Signed-off-by: zengqiang.fang <zengqiang.fang@thoughtworks.com>

--- code/reactiveWebApplication/src/main/java/com/example/reactivewebapplication/GreetingHandler.java
+++ code/reactiveWebApplication/src/main/java/com/example/reactivewebapplication/GreetingHandler.java
@@ -1,4 +1,5 @@
 package com.example.reactivewebapplication;
+import com.sun.media.sound.InvalidDataException;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -7,11 +8,21 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Component
 public class GreetingHandler {
 
     public Mono<ServerResponse> hello(ServerRequest request) {
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(new Greeting("Hello, Spring!")));
+    }
+
+    public Mono<ServerResponse> divide(ServerRequest serverRequest){
+        Optional<String> param1 = serverRequest.queryParam("param1");
+        Optional<String> param2 = serverRequest.queryParam("param2");
+
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(Integer.parseInt(param1.get())/Integer.parseInt(param2.get())));
     }
 }
--- code/reactiveWebApplication/src/main/java/com/example/reactivewebapplication/GreetingRouter.java
+++ code/reactiveWebApplication/src/main/java/com/example/reactivewebapplication/GreetingRouter.java
@@ -3,12 +3,12 @@ package com.example.reactivewebapplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration(proxyBeanMethods = false)
 public class GreetingRouter {
@@ -18,5 +18,13 @@ public class GreetingRouter {
 
         return RouterFunctions
                 .route(GET("/hello").and(accept(MediaType.APPLICATION_JSON)), greetingHandler::hello);
+
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> operatorRoute(GreetingHandler greetingHandler){
+        return RouterFunctions.route()
+                .GET("/divide", RequestPredicates.queryParam("param1", t->true).
+                        and(queryParam("param2", t->true)).and(accept(MediaType.APPLICATION_JSON)), greetingHandler::divide).build();
     }
 }
