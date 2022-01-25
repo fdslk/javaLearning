a3d669193b85d0a8b99a9b42194aa54ff2c4c31c
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-01-25 09:36:09 +0800

    添加slf4j maven dependency
    Signed-off-by: zengqiang.fang <zengqiang.fang@thoughtworks.com>

--- code/reactiveWebApplication/src/main/java/com/example/reactivewebapplication/FilterRest.java
+++ code/reactiveWebApplication/src/main/java/com/example/reactivewebapplication/FilterRest.java
@@ -1,5 +1,6 @@
 package com.example.reactivewebapplication;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.codec.multipart.Part;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import java.util.TreeMap;
 
 @RestController
 @RequestMapping("/testrouter")
+@Slf4j
 public class FilterRest implements RouterFunction {
     @GetMapping("/test")
     public Mono<String> test() {
@@ -27,13 +29,17 @@ public class FilterRest implements RouterFunction {
     public Mono<Map<String, Object>> test1(ServerWebExchange exchange) {
         Mono<MultiValueMap<String, String>> formData = exchange.getFormData();
         Mono<MultiValueMap<String, Part>> multipartData = exchange.getMultipartData();
+
+        String logPrefix = exchange.getLogPrefix();
+        log.info(logPrefix);
         Mono<Map<String, Object>> map = Mono.zip(formData, multipartData)
                 .map(tuple -> {
                     Map<String, Object> result = new TreeMap<>();
-                    tuple.getT1().forEach((key, values) -> result.put(key, values.get(0)));
-                    tuple.getT2().forEach((key, values) -> result.put(key, values.get(0).toString()));
+                    tuple.getT1().forEach((key, values) -> result.put(key, values));
+                    tuple.getT2().forEach((key, values) -> result.put(key, values));//TODO: can be serialized
                     return result;
                 });
+
         return map;
 //        return Mono.just("RequestMappingHandlerMapping");
     }
