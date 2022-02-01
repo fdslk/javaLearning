824ccf27d7f20e321263cec99af70af358054512
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-02-01 21:57:19 +0800

    添加flux or mono combination publisher测试
    Signed-off-by: zengqiang.fang <zengqiang.fang@thoughtworks.com>

--- code/reactiveWebApplication/src/test/java/com/example/reactivewebapplication/CombinationPublisherUtilTests.java
+++ code/reactiveWebApplication/src/test/java/com/example/reactivewebapplication/CombinationPublisherUtilTests.java
@@ -2,15 +2,20 @@ package com.example.reactivewebapplication;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CombinationPublisherUtilTests {
     private final CombinationPublisherUtil combinationPublisherUtil;
+    private final Flux<Integer> oddNumbers;
+    private final Flux<Integer> evenNumbers;
 
     public CombinationPublisherUtilTests() {
         combinationPublisherUtil = new CombinationPublisherUtil();
+        evenNumbers = combinationPublisherUtil.createEvenNumbers(0, 5);
+        oddNumbers = combinationPublisherUtil.createOddNumbers(0, 5);
     }
 
     @Test
@@ -31,14 +36,58 @@ public class CombinationPublisherUtilTests {
 
     @Test
     void givenFluxes_whenConcatIsInvoked_ThenConcat(){
-        Flux<Integer> oddNumbers = combinationPublisherUtil.createOddNumbers(0, 5);
-        Flux<Integer> evenNumbers = combinationPublisherUtil.createEvenNumbers(0, 5);
-        StepVerifier.create(Flux.concat(evenNumbers, oddNumbers))
+        Flux<Integer> integerFlux = evenNumbers.concatWith(oddNumbers);
+//        StepVerifier.create(Flux.concat(evenNumbers, oddNumbers))
+        StepVerifier.create(integerFlux)
                 .assertNext(t->assertThat(t).isEqualTo(0))
                 .assertNext(t->assertThat(t).isEqualTo(2))
                 .assertNext(t->assertThat(t).isEqualTo(4))
                 .assertNext(t->assertThat(t).isEqualTo(1))
                 .assertNext(t->assertThat(t).isEqualTo(3))
                 .verifyComplete();
+    }
+
+    @Test
+    void givenFluxes_whenCombineLastedIsInvoked_thenCombineLasted(){
+        StepVerifier.create(Flux.combineLatest(evenNumbers, oddNumbers, (a, b) -> a + b))
+                .assertNext(t -> assertThat(t).isEqualTo(5))
+                .assertNext(t -> assertThat(t).isEqualTo(7))
+                .verifyComplete();
+    }
+
+    @Test
+    void givenFluxes_WhenMergeIsInvoked_thenMerge(){
+        StepVerifier.create(Flux.merge(evenNumbers, oddNumbers))
+                .assertNext(t->assertThat(t).isEqualTo(0))
+                .assertNext(t->assertThat(t).isEqualTo(2))
+                .assertNext(t->assertThat(t).isEqualTo(4))
+                .assertNext(t->assertThat(t).isEqualTo(1))
+                .assertNext(t->assertThat(t).isEqualTo(3))
+                .verifyComplete();
+    }
+
+    @Test
+    public void givenFluxes_whenZipIsInvoked_thenZip() {
+        Flux<Integer> fluxOfIntegers = Flux.zip(
+                evenNumbers,
+                oddNumbers,
+                (a, b) -> a + b);
+
+        StepVerifier.create(fluxOfIntegers)
+                .expectNext(1) // 0 + 1
+                .expectNext(5) // 2 + 3
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void givenMono_whenZipIsInvoked_thenZip() {
+        Mono<String> firstMono = Mono.just("one");
+        Mono<String> secondMono = Mono.just("two");
+
+        StepVerifier.create(Mono.zip(firstMono, secondMono, (a, b) -> a +b))
+                .expectNext("onetwo") // 0 + 1
+                .expectComplete()
+                .verify();
     }
 }
