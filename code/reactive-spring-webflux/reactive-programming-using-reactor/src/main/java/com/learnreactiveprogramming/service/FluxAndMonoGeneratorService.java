0031067f07d4822ca74d065e353a7ad7345ced5a
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-02-11 15:52:25 +0800

    添加map对于flux的操作以及测试
    Signed-off-by: zengqiang.fang <zengqiang.fang@thoughtworks.com>

--- code/reactive-spring-webflux/reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ code/reactive-spring-webflux/reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -3,7 +3,9 @@ package com.learnreactiveprogramming.service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class FluxAndMonoGeneratorService {
 
@@ -17,6 +19,70 @@ public class FluxAndMonoGeneratorService {
     public Mono<String> nameMono() {
         return Mono.just("ddd");
     }
+
+    public Flux<String> upperCaseNameFlux(){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aaa");
+        strings.add("bbb");
+        strings.add("ccc");
+        return Flux.fromIterable(strings).map(String::toUpperCase).log();
+    }
+
+    public Flux<String> upperCaseNameFlux_immutability(){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aaa");
+        strings.add("bbb");
+        strings.add("ccc");
+        Flux<String> stringFlux = Flux.fromIterable(strings);
+        stringFlux.map(String::toUpperCase);
+        return stringFlux;
+    }
+
+    public Flux<String> upperCaseNameFlux_filter(int stringLength){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aa");
+        strings.add("bbb");
+        strings.add("ccc");
+        return Flux.fromIterable(strings)
+                .filter(s -> s.length() > stringLength)
+                .map(String::toUpperCase);
+    }
+
+    public Flux<String> nameFlux_flatMap(int stringLength){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aa");
+        strings.add("bbb");
+        strings.add("ccc");
+        return Flux.fromIterable(strings)
+                .filter(s -> s.length() > stringLength)
+                .map(String::toUpperCase)
+                .flatMap(s -> splitName(s));
+    }
+
+    public Flux<String> nameFlux_flatMapSequential_async(int stringLength){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aa");
+        strings.add("bbb");
+        strings.add("ccc");
+        return Flux.fromIterable(strings)
+                .filter(s -> s.length() > stringLength)
+                .map(String::toUpperCase)
+                .flatMapSequential(s -> splitName_withDelay(s)).log();
+    }
+
+
+    private Flux<String> splitName(String s) {
+        String[] split = s.split("");
+        return Flux.fromArray(split);
+    }
+
+    private Flux<String> splitName_withDelay(String s) {
+        String[] split = s.split("");
+        int i = new Random().nextInt(1000);
+        return Flux.fromArray(split)
+                .delayElements(Duration.ofMillis(i));
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService.nameFlux()
--- code/reactive-spring-webflux/reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ code/reactive-spring-webflux/reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -16,8 +16,47 @@ class FluxAndMonoGeneratorServiceTest {
     void givenFlux_WhennameFLuxIsInvoked_ThenReturnFlux(){
         Flux<String> nameFlux = fluxAndMonoGeneratorService.nameFlux();
         StepVerifier.create(nameFlux)
-        .expectNext("aaa", "bbb", "cccc")
+        .expectNext("aaa", "bbb", "ccc")
         .verifyComplete();
     }
 
+    @Test
+    void upperCaseNameFlux() {
+        Flux<String> nameFlux = fluxAndMonoGeneratorService.upperCaseNameFlux();
+        StepVerifier.create(nameFlux)
+                .expectNext("AAA", "BBB", "CCC")
+                .verifyComplete();
+    }
+
+    @Test
+    void upperCaseNameFlux_Immutability() {
+        Flux<String> nameFlux = fluxAndMonoGeneratorService.upperCaseNameFlux_immutability();
+        StepVerifier.create(nameFlux)
+                .expectNext("aaa", "bbb", "ccc")
+                .verifyComplete();
+    }
+
+    @Test
+    void upperCaseNameFlux_Filter() {
+        Flux<String> nameFlux = fluxAndMonoGeneratorService.upperCaseNameFlux_filter(2);
+        StepVerifier.create(nameFlux)
+                .expectNext("BBB", "CCC")
+                .verifyComplete();
+    }
+
+    @Test
+    void nameFlux_flatMap() {
+        Flux<String> nameFlux = fluxAndMonoGeneratorService.nameFlux_flatMap(2);
+        StepVerifier.create(nameFlux)
+                .expectNext("B", "B", "B", "C", "C", "C")
+                .verifyComplete();
+    }
+
+    @Test
+    void nameFlux_flatMap_async() {
+        Flux<String> nameFlux = fluxAndMonoGeneratorService.nameFlux_flatMapSequential_async(2);
+        StepVerifier.create(nameFlux)
+                .expectNext("B", "B", "B", "C", "C", "C")
+                .verifyComplete();
+    }
 }
