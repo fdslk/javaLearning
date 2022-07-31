a396b3071ef93472ca6cb0344ea05b35ee4d54af
zengqiang.fang <zqfangmaster@163.com>
2022-07-31 19:12:51 +0800

    添加@Operation注释endpointsummary

--- code/openapidemo/src/main/java/com/openapi/demo/BookController.java
+++ code/openapidemo/src/main/java/com/openapi/demo/BookController.java
@@ -1,5 +1,6 @@
 package com.openapi.demo;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,13 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/{bookName}")
+    @Operation(summary = "Get a book by its name")
     public Mono<Book> getBookByName(@PathVariable String bookName) {
         return bookService.getBookByName(bookName);
     }
 
     @GetMapping("/filter")
+    @Operation(summary = "Get book list by price range")
     public Mono<ResponseEntity> filterBooks(@RequestParam Integer minPrice, @RequestParam Integer maxPrice) {
         return bookService.getBookByPriceRange(minPrice, maxPrice)
                 .collectList().flatMap(
