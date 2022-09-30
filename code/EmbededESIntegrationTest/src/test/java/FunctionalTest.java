89c2c536993127ba6d7de2b332618305ab637d29
zengqiang.fang <zqfangmaster@163.com>
2022-09-30 09:08:00 +0800

    升级 rest-high-level-client依赖版本

--- code/EmbededESIntegrationTest/src/test/java/FunctionalTest.java
+++ code/EmbededESIntegrationTest/src/test/java/FunctionalTest.java
@@ -1,4 +1,12 @@
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -8,18 +16,25 @@ public class FunctionalTest extends EmbeddedESBaseTest{
     public void shouldRunEmbeddedES() throws IOException {
         System.out.println(111);
 
-//        SearchRequest searchRequest = new SearchRequest("person");
-//        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-//
-//        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("name", "Bill");
-//        searchSourceBuilder.query(matchQueryBuilder);
-//        searchRequest.source(searchSourceBuilder);
-//
-//
-//        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(
+                        HttpHost.create("127.0.0.1:9201")
+                )
+        );
 
-        SearchResponse person = runner.search("person", null, null, 0, 100);
+        SearchRequest searchRequest = new SearchRequest("person");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("name", "Bill");
+        searchSourceBuilder.query(matchQueryBuilder);
+        searchRequest.source(searchSourceBuilder);
+
+
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchResponse person = runner.search("person",null, null, 0, 100);
 
         System.out.println(person.getHits().getTotalHits());
+        System.out.println(searchResponse.getHits().getTotalHits());
     }
 }
